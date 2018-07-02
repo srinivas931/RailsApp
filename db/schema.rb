a73e84b03c874546b875c760614e01648960448f
srinivas931 <srinivasreddypakanati93@gmail.com>
2018-07-02 17:06:10 -0400

    Added Microposts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_194812) do
+ActiveRecord::Schema.define(version: 2018_07_02_203645) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "context"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
